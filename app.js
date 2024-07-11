785d46aba04c02cfc89ecf0ce8d6f28c8eecbe4f
Sahilh1212 <sahilhirani01|@gmail.com>
2024-07-11 14:41:14 +0530

    final

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const userModel = require('./model/user')
+const cookieParser = require('cookie-parser')
 
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cookieParser())
+
+
 
 app.get('/', (req, res) => {
     res.render('index')
 })
+app.get('/test', (req, res) => {
+    res.render('multer')
+})
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log(req.body);
+    res.redirect('test')
+})
+
+
 app.get('/read', async (req, res) => {
     let users = await userModel.find()
     res.render('read', { users })
